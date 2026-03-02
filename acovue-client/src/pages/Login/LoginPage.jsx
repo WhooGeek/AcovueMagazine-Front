7b22944f4e376b57a94d0be91692d4f7373b7cd1
WhooGeek <rlarjswn0303@naver.com>
2026-03-02 13:46:06 +0900

    FIX : OAuth 리다이렉팅 주소 변경

--- acovue-client/src/pages/Login/LoginPage.jsx
+++ acovue-client/src/pages/Login/LoginPage.jsx
@@ -34,13 +34,13 @@ export default function LoginPage() {
     // 구글 로그인 핸들러
     const handleGoogleLogin = () => {
       // 구글 로그인 URL로 리다이렉트
-      window.location.href = "/oauth2/authorization/google";
+      window.location.href = "http://52.78.166.206/oauth2/authorization/google";
     }
 
     // 네이버 로그인 핸들로
     const handleNaverLogin = () => {
       // 네이버 로그인 URL로 리다이렉트
-      window.location.href = "/oauth2/authorization/naver";
+      window.location.href = "http://52.78.166.206/oauth2/authorization/naver";
     }
 
   return (
